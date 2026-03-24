85ac582babcb304bf8bea435b14cfaed50186420
Wojciech Sierota <wojciech.sierota.s@gmail.com>
2026-03-24 15:51:22 +0100

    Complete Day 01 - SELECT and WHERE filtering basics

--- Day_01_SELECT_Basics/exercises.sql
+++ Day_01_SELECT_Basics/exercises.sql
@@ -1,1 +1,18 @@
--- My first SQL exercises 
+-- TASK 1: Everything
+SELECT *
+FROM Track;
+-- TASK 2: Two columns together
+SELECT Name,
+    Composer
+FROM Track;
+-- TASK 3: Two columns with limit
+SELECT Name,
+    UnitPrice
+FROM Track
+LIMIT 15;
+-- TASK 4: Alias
+SELECT Name AS Song_Title
+FROM Track;
+-- TASK 5: Unique composers
+SELECT DISTINCT Composer
+FROM Track;
